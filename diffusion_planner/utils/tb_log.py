3bb505ea7834295528fd8a132ffbb202189f876a
Guoqi Li <95746710+vodermot0010@users.noreply.github.com>
2026-03-20 16:55:05 +0800

    Upload diffusion_planner/utils/tb_log.py

---
+++ diffusion_planner/utils/tb_log.py
@@ -0,0 +1,51 @@
+import os
+import importlib
+from torch.utils.tensorboard import SummaryWriter
+
+try:
+    wandb = importlib.import_module("wandb")
+except ModuleNotFoundError:
+    wandb = None
+
+class TensorBoardLogger():
+    def __init__(self, run_name, notes, args, wandb_resume_id, save_path, rank=0):
+        """
+        project_name (str): wandb project name
+        config: dict or argparser
+        """              
+        self.args = args
+        self.writer = None
+        self.id = None
+        
+        if rank == 0:
+            if args.use_wandb:
+                if wandb is None:
+                    print("[WARN] use_wandb=True but wandb is not installed, fallback to tensorboard only.")
+                else:
+                    os.environ["WANDB_MODE"] = "online"
+                    wandb_writer = wandb.init(
+                        project='Diffusion-Planner',
+                        name=run_name,
+                        notes=notes,
+                        resume="allow",
+                        id=wandb_resume_id,
+                        sync_tensorboard=True,
+                        dir=f'{save_path}',
+                    )
+                    wandb.config.update(args)
+                    self.id = wandb_writer.id
+
+            self.writer = SummaryWriter(log_dir=f'{save_path}/tb')
+    
+    def log_metrics(self, metrics: dict, step: int):
+       """
+       metrics (dict):
+       step (int, optional): epoch or step
+       """
+       if self.writer is not None:
+            for key, value in metrics.items():
+                self.writer.add_scalar(key, value, step)
+
+    def finish(self):
+       if self.writer is not None:
+            self.writer.close()
